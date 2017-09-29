5382a4a2785847defb9c79b8a8274e45241c4a81
saviodo5591 <jcharney95@gmail.com>
2017-09-29 17:30:52 -0500

    Initial Commit

---
+++ data/types/game.js
@@ -0,0 +1,12 @@
+const Game =`
+  type Game {
+    name: String!
+    id: String
+    description: String
+    genre: String
+    primaryPlaystyle: String
+    playerbase: String
+  }
+`;
+
+export default Game;
---
+++ data/types/reward.js
@@ -0,0 +1,10 @@
+const Reward =`
+  type Reward {
+    id: String
+    name: String
+    description: String
+    type: String
+  }
+`;
+
+export default Reward;
